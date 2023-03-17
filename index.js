601ccd8b335520289d049aee4f39df0c6cbae7d1
firalammii <90164296+firalammii@users.noreply.github.com>
2023-03-18 02:26:59 +0300

    for dep

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ import bodyParser from "body-parser";
 import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
 import routerPosts from './routes/posts.js'
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit:"30mb", extenteded: true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send("Hello World this a memories project API");
+})
+
 app.use('/posts', routerPosts)
 
-const DB_CONNECTION_URI = 'mongodb+srv://firalammy:7ONMtuZOuf5fMlWl@cluster0.n8gmagp.mongodb.net/?retryWrites=true&w=majority';
 // const DB_CONNECTION_URI = 'mongodb://localhost/memories_project'
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(DB_CONNECTION_URI, {useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(process.env.DB_CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() =>app.listen(PORT, () => console.log(`succesfull connection to db PORT ${PORT} ....`)))
 .catch(err => console.log( err +'\nunable to connect'))
